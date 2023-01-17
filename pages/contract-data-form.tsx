647eadeb846dd8f6c8ded772164b811586480157
mme <9083787+mme022@users.noreply.github.com>
2023-01-17 20:51:27 +0000

    initial commit

---
+++ pages/contract-data-form.tsx
@@ -0,0 +1,119 @@
+import Link from "next/link";
+import { fromJSON } from "postcss";
+import { FormEvent } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function ContractDataForm() {
+  // Handle the submit event on form submit.
+  const handleSubmit = async (event: FormEvent) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    // Cast the event target to an html form
+    const form = event.target as HTMLFormElement;
+
+    // Get data from the form.
+    const data = {
+      researcher: form.first.value as string, //the name of the lead reasearcher
+      university: form.university.value as string, //the name of their university
+      patent_filed: {
+        patent_id: form.patend_id.value as string, //must follow the pattern "[A-F]-[1-9]{5,7}\/[A-Z]{5,9}", e.g. A-12345/HAYFEVER
+        institution: form.institution.value as string, //the name of the filing institution
+      },
+    };
+
+    // Send the form data to our API and get a response.
+    const response = await fetch("/api/form", {
+      // Body of the request is the JSON data we created above.
+      body: JSON.stringify(data),
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // The method is POST because we are sending data.
+      method: "POST",
+    });
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+    alert(`Is this your full name: ${result.data}`);
+  };
+  return (
+    <div className="container">
+      <h1 className={styles.title}>
+        Form <Link href="/">with</Link> JavaScript.
+      </h1>
+
+      <p className={styles.description}>
+        Get started by looking at{" "}
+        <code className={styles.code}>pages/js-form.js</code>
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="researcher">Reseacher</label>
+        <input
+          type="text"
+          id="researcher"
+          name="researcher"
+          className="mt-1
+          block
+          w-full
+          rounded-md
+          border-gray-300
+          shadow-sm
+          focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          required
+        />
+        <label htmlFor="university">University</label>
+        <input
+          type="text"
+          id="university"
+          name="university"
+          className="mt-1
+          block
+          w-full
+          rounded-md
+          border-gray-300
+          shadow-sm
+          focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          required
+        />
+        <label htmlFor="patent_id">Patent ID</label>
+        <input
+          type="text"
+          id="patent_id"
+          name="patent_id"
+          className="mt-1
+          block
+          w-full
+          rounded-md
+          border-gray-300
+          shadow-sm
+          focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          required
+        />
+        <label htmlFor="institution">Institution</label>
+        <input
+          type="text"
+          id="institution"
+          name="institution"
+          className="mt-1
+          block
+          w-full
+          rounded-md
+          border-gray-300
+          shadow-sm
+          focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          required
+        />
+        <button
+          type="submit"
+          className="rounded-md shadow-sm w-full mt-1 bg-indigo-600"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Header from "./header";
 
 export default function IndexPage() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next.js forms</title>
         <meta name="description" content="Learn forms with Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Forms with <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Header />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Coding challenge for <a href="https://molecule.to">Molecule!</a>
+          </h1>
 
-        <p className={styles.description}>
-          Get started by looking at{' '}
-          <code className={styles.code}>pages/js-form.js</code> and{' '}
-          <code className={styles.code}>pages/no-js-form.js</code>
-        </p>
+          <div className={styles.grid}>
+            <Link href="/contract-data-form" className={styles.card}>
+              <h2>Create and encrypt contract data;</h2>
+              <p>
+                Upload encrypted contract data to a decentralised storage before
+                minting NFT.
+              </p>
+            </Link>
+            <Link href="/js-form" className={styles.card}>
+              <h2>Form with JavaScript &rarr;</h2>
+              <p>Learn to handle forms with JavaScript in Next.js.</p>
+            </Link>
+          </div>
+        </main>
 
-        <div className={styles.grid}>
-          <Link href="/js-form" className={styles.card}>
-            <h2>Form with JavaScript &rarr;</h2>
-            <p>Learn to handle forms with JavaScript in Next.js.</p>
-          </Link>
-
-          <Link href="/no-js-form" className={styles.card}>
-            <h2>Form without JavaScript &rarr;</h2>
-            <p>Learn to handle forms without JavaScript in Next.js.</p>
-          </Link>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
-          Built with Next.js | Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <footer className={styles.footer}></footer>
+      </div>
     </div>
-  )
+  );
 }
